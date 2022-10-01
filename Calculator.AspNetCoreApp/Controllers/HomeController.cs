07b0f9ca6a1318215f716bc6d406762e768c659a
Konrad Tarnowski <ktarnowski08@gmail.com>
2022-10-01 18:57:50 +0200

    Created calculator logic.

--- Calculator.AspNetCoreApp/Controllers/HomeController.cs
+++ Calculator.AspNetCoreApp/Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 ﻿using Calculator.AspNetCoreApp.Models;
 using Microsoft.AspNetCore.Mvc;
+using System.Data;
 using System.Diagnostics;
 
 namespace Calculator.AspNetCoreApp.Controllers
@@ -7,6 +8,7 @@ namespace Calculator.AspNetCoreApp.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private DataTable _dataTable = new DataTable();
 
         public HomeController(ILogger<HomeController> logger)
         {
@@ -27,6 +29,20 @@ namespace Calculator.AspNetCoreApp.Controllers
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+        [HttpPost]
+        public IActionResult Result(string inputVal)
+        {
+            try
+            {
+                var result = Math.Round(Convert.ToDouble(_dataTable.Compute(inputVal.Replace(",", "."), "")), 2).ToString();
+                return Json(new { success = true, result });
+            }
+            catch (Exception exception)
+            {
+                return Json(new { success = false, message = exception.Message });
+                throw;
+            }
         }
     }
 }
